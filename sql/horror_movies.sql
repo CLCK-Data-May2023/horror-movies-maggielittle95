8ede3a6099ad8c4c44964a5641bcf373a74f5709
Maggie  Little <maggielittle95@gmail.com>
2023-10-12 11:03:40 -0400

    horror movies sql file

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,6 @@
 -- Add your SQL here
+SELECT id as Movie_ID, name as Movie_Title, imdb_rating as Rating, genre, year
+FROM movies
+WHERE genre = 'horror' AND year <= 1985
+ORDER BY imdb_rating DESC
+LIMIT 3;
